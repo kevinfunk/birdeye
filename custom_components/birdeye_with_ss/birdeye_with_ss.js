95863c8f9da436667dec378286b31de65b23f86d
Kevin Funk <kevin.funk@acquia.com>
2023-03-06 08:12:07 -0500

    Adds SS custom components.

---
+++ custom_components/birdeye_with_ss/birdeye_with_ss.js
@@ -0,0 +1,26 @@
+let With = createReactClass({
+  render: function () {
+    let display = 610;
+    return (
+      <div>
+        <Display display={display} />
+      </div>
+    )
+  }
+});
+
+
+let Display = createReactClass({
+  render: function () {
+    return (
+      <div className="display">
+        {this.props.display}
+      </div>
+    )
+  }
+});
+
+
+ReactDOM.render(<With />,
+  document.getElementById('birdeye_with')
+);
